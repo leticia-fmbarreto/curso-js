26223dbe65d542aba33d331575875fd797f59042
leticia-fmbarreto <leticia123fm@gmail.com>
2025-08-07 23:25:57 -0300

    aula 12 ex 15

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'um Homem'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src','pfoto-bebe-m.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'pfoto-homem-m.png')
+            } else if (idade< 50) {
+                //Adulto
+                img.setAttribute('src', 'pfoto-homem-j.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'pfoto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = ' uma Mulher'
+            if (idade >=0 && idade < 10) {
+                //Criança
+                 img.setAttribute('src', 'pfoto-bebe-f.png')
+            } else if (idade < 21) {
+                //Jovem
+                 img.setAttribute('src', 'pfoto-mulher-j.png')
+            } else if (idade< 50) {
+                //Adulto
+                 img.setAttribute('src', 'pfoto-mulher-v.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'pfoto-mulher-v.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
